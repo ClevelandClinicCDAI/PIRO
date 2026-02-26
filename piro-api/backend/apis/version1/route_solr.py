a2070f77d260fabf377ebb4c1d692562f87a778c
Scott Robertson <roberts10@ccf.org>
2026-02-26 12:41:29 -0500

    Comment out MIcroscopic description options in Main search and advanced searcH

--- piro-api/backend/apis/version1/route_solr.py
+++ piro-api/backend/apis/version1/route_solr.py
@@ -293,7 +293,8 @@ async def filter_details():
         {
             "name": "All Text Fields",
             "type": "text",
-            "value": "final;comment;addend;microscopic",
+            # "value": "final;comment;addend;microscopic",
+            "value": "final;comment;addend",
             "show": "true",
             "placeholder": "Search text across all comment fields",
             "color": "#A52A2A",
@@ -353,13 +354,13 @@ async def filter_details():
             "show": "true",
             "color": "#954535",
         },
-        {
-            "name": "Microscopic Description",
-            "type": "text",
-            "value": "microscopic",
-            "show": "true",
-            "color": "#6B8E23",
-        },
+        # {
+        #     "name": "Microscopic Description",
+        #     "type": "text",
+        #     "value": "microscopic",
+        #     "show": "true",
+        #     "color": "#6B8E23",
+        # },
         {
             "name": "Pathologist",
             "type": "suggest",
@@ -584,11 +585,11 @@ async def filter_advanced_details():
             "type": "string",
             "operators": ["contains", "not"],
         },
-        "microscopic": {
-            "name": "Microscopic Description",
-            "type": "string",
-            "operators": ["contains", "not"],
-        },
+        # "microscopic": {
+        #     "name": "Microscopic Description",
+        #     "type": "string",
+        #     "operators": ["contains", "not"],
+        # },
         "synoptic": {
             "name": "Synoptic Report",
             "type": "string",
